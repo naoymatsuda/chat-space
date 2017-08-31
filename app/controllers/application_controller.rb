3f6dd8f2c5ac904d07fc14ed1e90c760dd0dc0f0
naoymatsuda <naoy.matsuda@gmail.com>
2017-08-31 16:32:18 +0900

    パス追加
    サインイン後、サインアウト後のパスを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 end
