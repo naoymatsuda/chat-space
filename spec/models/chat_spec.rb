accf998a1843d8ef594f42058bea14c43062e25a
naoymatsuda <naoy.matsuda@gmail.com>
2017-09-12 15:25:42 +0900

    change method
    メソッドをcreateからbuildへ変更

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -3,30 +3,28 @@ require 'rails_helper'
 describe Chat do
   describe '#create' do
     it "is valid with a body of chat" do
-      chat = create(:chat, image: nil)
+      chat = build(:chat, image: nil)
       expect(chat).to be_valid
     end
   end
 
   describe '#create' do
     it "is valid with a body of image" do
-      chat = create(:chat, text: nil)
+      chat = build(:chat, text: nil)
       expect(chat).to be_valid
     end
   end
 
   describe '#create' do
     it "is valid with a body of chas and image" do
-      chat = create(:chat)
+      chat = build(:chat)
       expect(chat).to be_valid
     end
   end
 
   describe '#create' do
     it "is valid without a chat and image" do
-      chat = create(:chat)
-      chat.text = nil
-      chat.image = nil
+      chat = build(:chat, text: nil, image: nil)
       chat.valid?
       expect(chat.errors[:text_or_image]).to include("can't be blank")
     end
@@ -34,7 +32,7 @@ describe Chat do
 
   describe '#create' do
     it "is valid without a group_id" do
-      chat = create(:chat)
+      chat = build(:chat)
       chat.group_id = nil
       chat.valid?
       expect(chat.errors[:group_id]).to include("can't be blank")
@@ -43,7 +41,7 @@ describe Chat do
 
   describe '#create' do
     it "is valid without a user_id" do
-      chat = create(:chat)
+      chat = build(:chat)
       chat.user_id = nil
       chat.valid?
       expect(chat.errors[:user_id]).to include("can't be blank")
