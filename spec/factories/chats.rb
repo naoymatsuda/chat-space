2a9fbac478d29ecc8825ba2b29b2bcb38f86b961
naoymatsuda <naoy.matsuda@gmail.com>
2017-09-12 15:13:07 +0900

    correcting mistakes

--- spec/factories/chats.rb
+++ spec/factories/chats.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :chat do
-    text "aaa"
+    text Faker::Lorem.sentence
     image Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image.jpg'))
     user_id 1
     group_id 1
