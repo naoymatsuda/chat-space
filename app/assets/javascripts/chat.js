36f72d1fc2ef3829d146e3b83f336262ac5c9eb5
naoymatsuda <naoy.matsuda@gmail.com>
2017-09-14 19:19:41 +0900

    fix function ajax message
    -chat controllerの修正
    -jbuilderの作成
    -ajax通信の作成

---
+++ app/assets/javascripts/chat.js
@@ -0,0 +1,45 @@
+$(function(){
+  function buildHTML(chat){
+    var html =
+    `<div class="messages">
+       <div class="upper-message">
+         <div class="upper-message__user-name">
+           ${chat.name}
+         </div>
+         <div class="upper-message__date">
+           ${chat.time}
+         </div>
+       </div>
+       <div class="lower-message">
+         <div class="lower-message__content">
+           ${chat.text}
+         </div>
+       </div>
+    </div>`;
+  return html;
+  }
+
+$(function () {
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    console.log(url);
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(chat){
+      console.log(chat);
+      var html = buildHTML(chat);
+      $('.messages_group').append(html);
+      $('#chat_text').val("");
+    })
+  })
+});
+});
---
+++ app/views/chats/create.json.jbuilder
@@ -0,0 +1,4 @@
+json.text @chat.text
+json.image @chat.image
+json.name @chat.user.name
+json.time @chat.created_at
